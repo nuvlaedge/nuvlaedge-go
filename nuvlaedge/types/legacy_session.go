fd0b3439bd3139ed0215b9c5ac211ea6a8b09f6c
Ignacio Penas Fernandez <11061525+ignacio-penas@users.noreply.github.com>
2024-06-11 15:10:11 +0200

    minor: Add parser from Session stored by NuvlaEdge python (#54)
    * minor: Add parser from Session stored by NuvlaEdge python
    
    * fix: Fix Legacy session file name and set default log level to info
    
    * fix: Update client version to  fix NuvlaEdge resource error
    
    * fix nil pointer

---
+++ nuvlaedge/types/legacy_session.go
@@ -0,0 +1,42 @@
+package types
+
+import (
+	"github.com/nuvla/api-client-go/clients"
+	"github.com/nuvla/api-client-go/common"
+	nuvlaTypes "github.com/nuvla/api-client-go/types"
+)
+
+type LegacySession struct {
+	Endpoint    string `json:"endpoint,omitempty"`
+	Insecure    bool   `json:"insecure,omitempty"`
+	Credentials struct {
+		Key    string `json:"key"`
+		Secret string `json:"secret"`
+		Href   string `json:"href"`
+	} `json:"credentials,omitempty"`
+
+	NuvlaEdgeUUID       string `json:"nuvlaedge-uuid,omitempty"`
+	NuvlaEdgeStatusUUID string `json:"nuvlaedge-status-uuid,omitempty"`
+}
+
+func (r *LegacySession) ConvertToNuvlaSession() *clients.NuvlaEdgeSessionFreeze {
+	f := &clients.NuvlaEdgeSessionFreeze{}
+	f.Credentials = &nuvlaTypes.ApiKeyLogInParams{
+		Key:    r.Credentials.Key,
+		Secret: r.Credentials.Secret,
+	}
+	f.NuvlaEdgeId = r.NuvlaEdgeUUID
+	f.NuvlaEdgeStatusId = r.NuvlaEdgeStatusUUID
+	f.Endpoint = r.Endpoint
+	f.Insecure = r.Insecure
+	// TODO: Need to fix this to true so the struct is created to prevent nil pointer error
+	f.PersistCookie = true
+	return f
+}
+
+func (r *LegacySession) Load(file string) error {
+	if err := common.ReadJSONFromFile(file, r); err != nil {
+		return err
+	}
+	return nil
+}
