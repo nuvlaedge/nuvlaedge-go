c1bda42e3feea428df5434212ac089fdaa846cd3
ignacio-penas <ignacio.penas4b@gmail.com>
2024-06-24 13:27:24 +0200

    test: adds pprof debugging and listener server in port 6060

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,7 +1,15 @@
 package main
 
-import "nuvlaedge-go/cli/cmd"
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"nuvlaedge-go/cli/cmd"
+)
 
 func main() {
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	cmd.Execute()
 }
