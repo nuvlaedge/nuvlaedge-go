8a012568814d43989a1bbe7e241ed35f6c3d1f41
ignacio-penas <ignacio.penas4b@gmail.com>
2024-06-25 11:04:41 +0200

    test: remove deployment state temporary

--- nuvlaedge/jobs/actions/deployment_state.go
+++ nuvlaedge/jobs/actions/deployment_state.go
@@ -1,7 +1,6 @@
 package actions
 
 import (
-	"encoding/json"
 	log "github.com/sirupsen/logrus"
 	"nuvlaedge-go/nuvlaedge/jobs/executors"
 )
@@ -11,26 +10,28 @@ type DeploymentState struct {
 }
 
 func (d *DeploymentState) ExecuteAction() error {
-	log.Infof("Deployment state action for deployment %s", d.deploymentId)
-	s, err := d.executor.GetServices()
-	temp, err := json.MarshalIndent(s, "", "  ")
-	log.Infof("Deployment %s services: \n%s", d.deploymentId, temp)
-	if err != nil {
-		log.Infof("Error getting services for deployment %s: %s", d.deploymentId, err)
-		return err
-	}
-	log.Infof("Deployment %s services: %v", d.deploymentId, s)
-	err = d.manageServiceParameters(s)
-	if err != nil {
-		log.Warnf("Error managing service parameters for deployment %s: %s", d.deploymentId, err)
-	}
-
-	err = d.executor.StateDeployment()
-	if err != nil {
-		log.Infof("Error getting deployment state for deployment")
-		return err
-	}
+	log.Infof("Dummy deployment state")
 	return nil
+	//log.Infof("Deployment state action for deployment %s", d.deploymentId)
+	//s, err := d.executor.GetServices()
+	//temp, err := json.MarshalIndent(s, "", "  ")
+	//log.Infof("Deployment %s services: \n%s", d.deploymentId, temp)
+	//if err != nil {
+	//	log.Infof("Error getting services for deployment %s: %s", d.deploymentId, err)
+	//	return err
+	//}
+	//log.Infof("Deployment %s services: %v", d.deploymentId, s)
+	//err = d.manageServiceParameters(s)
+	//if err != nil {
+	//	log.Warnf("Error managing service parameters for deployment %s: %s", d.deploymentId, err)
+	//}
+	//
+	//err = d.executor.StateDeployment()
+	//if err != nil {
+	//	log.Infof("Error getting deployment state for deployment")
+	//	return err
+	//}
+	//return nil
 }
 
 func (d *DeploymentState) GetExecutorName() executors.ExecutorName {
