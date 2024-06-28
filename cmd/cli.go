0ef2f92b0ec61d3f7ae31c0deb56384495aa3681
ignacio-penas <ignacio.penas4b@gmail.com>
2024-06-28 09:29:14 +0200

    test: Add debug pprof tag import conditional build

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	_ "net/http/pprof"
 	"nuvlaedge-go/cli/cmd"
 )
 
 func main() {
-	go func() {
-		fmt.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
 	cmd.Execute()
 }
