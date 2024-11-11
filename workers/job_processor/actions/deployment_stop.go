2784b5ceba6d2b0ae7d985bc9b1bec2b05f74f87
Ignacio Penas Fernandez <11061525+ignacio-penas@users.noreply.github.com>
2024-11-11 09:08:15 +0100

    fix(Job-Engine): Fixed a bug on the timeout of deployment stop (#99)

--- workers/job_processor/actions/deployment_stop.go
+++ workers/job_processor/actions/deployment_stop.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/nuvla/api-client-go/clients/resources"
 	log "github.com/sirupsen/logrus"
+	"nuvlaedge-go/common/constants"
 	"nuvlaedge-go/workers/job_processor/executors"
+	"time"
 )
 
 type DeploymentStop struct {
@@ -16,7 +18,7 @@ func (d *DeploymentStop) ExecuteAction(ctx context.Context) error {
 	defer CloseDeploymentClientWithLog(d.client)
 	defer d.executor.Close()
 
-	ctxTimed, cancel := context.WithTimeout(ctx, 60)
+	ctxTimed, cancel := context.WithTimeout(ctx, constants.DefaultJobTimeout*time.Second)
 	defer cancel()
 
 	if err := d.client.SetState(ctxTimed, resources.StateStopping); err != nil {
